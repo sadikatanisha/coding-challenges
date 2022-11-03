ce5c1284685147f9ef5dbdcbea2cb295e22d68f8
sadikatanisha <sadikatanisha@gmail.com>
2022-11-03 18:32:44 +0600

    added some lines

---
+++ 8kyu/only-need-one.js
@@ -0,0 +1,10 @@
+//You only need one - Beginner
+// will be given an array a and a value x. All you need to do is check whether the provided array contains the value
+
+//my solution:
+function check(a, x) {
+    // your code here
+    return a.includes(x)
+  }
+
+//best pracices:
