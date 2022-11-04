f135aa4eaa4dcf1e544c47d274dcfd394226feba
sadikatanisha <sadikatanisha@gmail.com>
2022-11-04 23:22:54 +0600

    new Challenges

---
+++ 8kyu/count-by-x.js
@@ -0,0 +1,9 @@
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+function countBy(x, n) {
+    var z = [];
+    for (i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+}
---
+++ 8kyu/sentence-smash.js
@@ -0,0 +1,7 @@
+
+//['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+//there can't be any spaces before or end of he string
+function smash (words) {
+
+    return words.join(" ").trim()
+ };
