7f92a3b5723847528d2a634bb2d638bec89efb3d
sadikatanisha <sadikatanisha@gmail.com>
2022-11-06 00:00:02 +0600

    new challenges

---
+++ 8kyu/count-he-monkeys.js
@@ -0,0 +1,11 @@
+// 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  1 --> [1]
+
+function monkeyCount(n) {
+    // your code here
+      let arr= []
+      for(let i=1;i<=n;i++){
+         arr.push(i)
+      }
+    return arr
+    }
