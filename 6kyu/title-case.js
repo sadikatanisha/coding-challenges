df230fac589ab48580145f64d781fa8d2b08b579
sadikatanisha <sadikatanisha@gmail.com>
2023-02-27 13:32:22 +0600

    reversing words in a string

--- 6kyu/title-case.js
+++ 6kyu/title-case.js
@@ -26,3 +26,22 @@ function titleCase(title, minorWords) {
   }
   return res.join(" ");
 }
+
+function titleCase(title, minorWords) {
+  var minorWords =
+    typeof minorWords !== "undefined"
+      ? minorWords.toLowerCase().split(" ")
+      : [];
+  return title
+    .toLowerCase()
+    .split(" ")
+    .map(function (v, i) {
+      if (v != "" && (minorWords.indexOf(v) === -1 || i == 0)) {
+        v = v.split("");
+        v[0] = v[0].toUpperCase();
+        v = v.join("");
+      }
+      return v;
+    })
+    .join(" ");
+}
