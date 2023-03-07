f810e7a62793098ff6d2b29cde5be7755ff379cc
sadikatanisha <sadikatanisha@gmail.com>
2023-03-07 23:50:16 +0600

    no oddities here

---
+++ 6kyu/10-minutes-walk.js
@@ -0,0 +1,20 @@
+function isValidWalk(walk) {
+    var dx = 0
+    var dy = 0
+    var dt = walk.length
+    
+    for (var i = 0; i < walk.length; i++) {
+      switch (walk[i]) {
+        case 'n': dy--; break
+        case 's': dy++; break
+        case 'w': dx--; break
+        case 'e': dx++; break
+      }
+    }
+    
+    return dt === 10 && dx === 0 && dy === 0
+  }
+
+
+
+  
---
+++ 7kyu/no-oddities.js
@@ -0,0 +1,6 @@
+function noOdds( values ){
+    // Return all non-odd values
+    return values.filter(el=> el%2==0)
+  }
+
+  
