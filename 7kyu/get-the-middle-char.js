9e042e6c754eba694346746851d7841b2a03c54a
sadikatanisha <sadikatanisha@gmail.com>
2023-04-20 10:03:03 +0600

    get the middle character

---
+++ 7kyu/get-the-middle-char.js
@@ -0,0 +1,27 @@
+//test cases
+    // Test.assertEquals(getMiddle("test"),"es");
+    // Test.assertEquals(getMiddle("testing"),"t");
+    // Test.assertEquals(getMiddle("middle"),"dd");
+    // Test.assertEquals(getMiddle("A"),"A");
+    function getMiddle(s)
+    {
+      let length = s.length 
+      if(length % 2!==0){
+        let start = (length-1)/2
+        return s.slice(start,start+1)
+        
+      }else{
+        let start =length/2-1
+        return s.slice(start,start+2)
+        
+      }
+      
+    }
+
+
+    //others solution
+
+    function getMiddle(s)
+{
+  return s.slice((s.length-1)/2, s.length/2+1);
+}
