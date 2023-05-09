b5c7fad3a45d1e06d74459e22617864862e27a85
sadikatanisha <sadikatanisha@gmail.com>
2023-05-09 22:38:22 +0600

    replace with alphabet position

---
+++ 6kyu/replacce-with-aalphabel-pos.js
@@ -0,0 +1,22 @@
+function alphabetPosition(text) {
+    var result = "";
+    for (var i = 0; i < text.length; i++){
+      var code = text.toUpperCase().charCodeAt(i)
+      if (code > 64 && code < 91) result += (code - 64) + " ";
+    }
+  
+    return result.slice(0, result.length-1);
+  }
+
+
+  //
+  function alphabetPosition(text) {
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    return text.toLowerCase().split('')
+    .filter( letter => {
+      let index = alphabet.indexOf(letter);
+      return index > -1;
+    }  )
+    .map( letter => alphabet.indexOf(letter) + 1 )
+    .join(' ')
+  }
