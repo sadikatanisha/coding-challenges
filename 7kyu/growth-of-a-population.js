c868bdf66654213b2af6b6d9d289f8c1ee942727
sadikatanisha <sadikatanisha@gmail.com>
2023-05-25 20:11:25 +0600

    growth

--- 7kyu/growth-of-a-population.js
+++ 7kyu/growth-of-a-population.js
@@ -5,3 +5,13 @@ function nbYear(p0, percent, aug, p) {
     }
     return years
   }
+
+
+  function nbYear(p0, percent, aug, p) {
+    var count = 0;
+    while (p0 < p) {
+      p0 = Math.floor((1 + percent / 100) * p0 + aug);
+      count++
+    } 
+    return count;
+  }
