1740fa9548d0743e53f60cbe95d8a2294c4d3360
sadikatanisha <sadikatanisha@gmail.com>
2023-06-01 13:37:10 +0600

    get sum

---
+++ 7kyu/get-sum.js
@@ -0,0 +1,23 @@
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+
+
+const GetSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+    return (max - min + 1) * (min + max) / 2;
+  }
+
+
+  function GetSum( a,b ) {
+    var result = 0;
+    var bigger = a > b ? a : b;
+    var smaller = a > b ? b : a;
+    for (var i = smaller; i <= bigger; i++) { result += i }
+    return result
+ }
