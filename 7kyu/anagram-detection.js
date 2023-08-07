bbaec2e92a2437745aef1cd2e0043c694edf3864
sadikatanisha <sadikatanisha@gmail.com>
2023-08-07 11:48:59 +0600

    anagram detection

---
+++ 7kyu/anagram-detection.js
@@ -0,0 +1,7 @@
+var isAnagram = function(test, original) {
+    var t = test.toLowerCase().split('').sort().join('');
+    var o = original.toLowerCase().split('').sort().join('');
+    return (t==o)?true:false;
+  };
+
+  
