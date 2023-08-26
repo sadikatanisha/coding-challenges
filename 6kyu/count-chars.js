d85bcd1d8f1e8b862a019e7be79fe6062a8ff40a
sadikatanisha <sadikatanisha@gmail.com>
2023-08-26 22:15:36 +0600

    count chars in string

---
+++ 6kyu/count-chars.js
@@ -0,0 +1,31 @@
+// DESCRIPTION:
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+
+function count(string) {
+  
+    let map = {
+      
+    }
+    for(let i=0;i<string.length;i++){
+      if(map[string[i]]){
+        map[string[i]] += 1
+      }else{
+        map[string[i]] = 1
+      }
+     
+    }
+    return map
+   
+  }
+
+
+  function count (string) {  
+    var count = {};
+    string.split('').forEach(function(s) {
+       count[s] ? count[s]++ : count[s] = 1;
+    });
+    return count;
+  }
