6852f18a32d6d6e6a35865eec3e50dda959aad1b
sadikatanisha <sadikatanisha@gmail.com>
2023-10-02 10:21:36 +0600

    my language skills

--- 7kyu/my-language-skills.js
+++ 7kyu/my-language-skills.js
@@ -15,3 +15,9 @@ function myLanguages(results) {
 }
 
 console.log(myLanguages({ Java: 10, Ruby: 80, Python: 65 }));
+
+function myLanguages(results) {
+  return Object.keys(results)
+    .filter((r) => results[r] > 59)
+    .sort((a, b) => results[b] - results[a]);
+}
