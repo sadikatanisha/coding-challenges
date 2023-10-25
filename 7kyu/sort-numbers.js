07376d809174733068ba853473556ce277a8e5cd
sadikatanisha <sadikatanisha@gmail.com>
2023-10-26 00:31:53 +0600

    sort numbers

---
+++ 7kyu/sort-numbers.js
@@ -0,0 +1,21 @@
+function solution(nums) {
+  if (nums) {
+    return nums.sort((a, b) => a - b);
+  } else {
+    return [];
+  }
+}
+
+function solution(nums) {
+  return nums
+    ? nums.sort(function (a, b) {
+        return a - b;
+      })
+    : [];
+}
+console.log(solution([1, 2, 3, 10, 5]), [1, 2, 3, 5, 10]);
+
+console.log(solution(null), []);
+console.log(solution([]), []);
+console.log(solution([20, 2, 10]), [2, 10, 20]);
+console.log(solution([2, 20, 10]), [2, 10, 20]);
