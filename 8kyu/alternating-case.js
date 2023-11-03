c7d3cfc90e41d39ec0035b50d570e901fecc1792
sadikatanisha <sadikatanisha@gmail.com>
2023-11-03 10:52:13 +0600

    alternating case

---
+++ 8kyu/alternating-case.js
@@ -0,0 +1,7 @@
+String.prototype.toAlternatingCase = function () {
+  return this.split("")
+    .map((char) =>
+      char == char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()
+    )
+    .join("");
+};
