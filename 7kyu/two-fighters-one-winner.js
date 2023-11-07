2f9e4d61b5248c58b7567d345324044464d8c83d
sadikatanisha <sadikatanisha@gmail.com>
2023-11-07 12:48:43 +0600

    two fighters another solution

--- 7kyu/two-fighters-one-winner.js
+++ 7kyu/two-fighters-one-winner.js
@@ -24,3 +24,14 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
     return firstAttacker;
   }
 }
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  while (fighter1.health > 0 && fighter2.health > 0) {
+    fighter2.health -= fighter1.damagePerAttack;
+    fighter1.health -= fighter2.damagePerAttack;
+  }
+
+  if (fighter1.health <= 0 && fighter2.health <= 0) return firstAttacker;
+  else if (fighter1.health <= 0) return fighter2.name;
+  else return fighter1.name;
+}
