a15280582d3759f1853f354bf225d81de0c7a0c5
sadikatanisha <sadikatanisha@gmail.com>
2023-11-18 00:34:33 +0600

    fix string case

--- 7kyu/fix-string-case.js
+++ 7kyu/fix-string-case.js
@@ -31,3 +31,9 @@ console.log(solve("code"), "code");
 console.log(solve("CODe"), "CODE");
 console.log(solve("COde"), "code");
 console.log(solve("Code"), "code");
+
+function solve(s) {
+  return s.replace(/[a-z]/g, "").length > s.length / 2
+    ? s.toUpperCase()
+    : s.toLowerCase();
+}
