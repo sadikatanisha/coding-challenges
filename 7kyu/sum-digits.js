0e04e371225e2c1f4a98d23a33e9385f24571ab8
sadikatanisha <sadikatanisha@gmail.com>
2023-11-24 23:07:30 +0600

    better solution

--- 7kyu/sum-digits.js
+++ 7kyu/sum-digits.js
@@ -13,3 +13,11 @@ console.log(sumDigits(10), 1);
 console.log(sumDigits(99), 18);
 console.log(sumDigits(-32), 5);
 console.log(sumDigits(58686), 42);
+function sumDigits(number) {
+  return Math.abs(number)
+    .toString()
+    .split("")
+    .reduce(function (a, b) {
+      return +a + +b;
+    }, 0);
+}
