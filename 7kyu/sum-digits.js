e2659f68e00eaf2d9add0ef83137bf353bcc4707
sadikatanisha <sadikatanisha@gmail.com>
2023-11-25 23:05:07 +0600

    sum digits

---
+++ 7kyu/sum-digits.js
@@ -0,0 +1,15 @@
+function sumDigits(number) {
+  let num = Math.abs(number).toString().split("");
+
+  // Check if the input number is not a valid number
+  if (isNaN(parseInt(num[0]))) {
+    return NaN;
+  }
+
+  return num.map((el) => parseInt(el)).reduce((a, b) => a + b, 0);
+}
+
+console.log(sumDigits(10), 1);
+console.log(sumDigits(99), 18);
+console.log(sumDigits(-32), 5);
+console.log(sumDigits(58686), 42);
