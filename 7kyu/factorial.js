64a4e8f4ce4912cca8096f0f703956c486199a04
sadikatanisha <sadikatanisha@gmail.com>
2023-12-05 00:49:12 +0600

    optimal solution

--- 7kyu/factorial.js
+++ 7kyu/factorial.js
@@ -20,3 +20,8 @@ console.log(factorial(2), 2, "factorial for 2 is 2");
 console.log(factorial(3), 6, "factorial for 3 is 6");
 console.log(factorial(5), 120, "factorial for 3 is 6");
 console.log(factorial(13), 120, "factorial for 3 is 6");
+
+function factorial(n) {
+  if (n < 0 || n > 12) throw new RangeError();
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
