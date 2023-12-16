e6be348f3c0ae540daa9cbdf424854c676cfd18e
sadikatanisha <sadikatanisha@gmail.com>
2023-12-16 15:40:13 +0600

    optimal sol

--- 7kyu/factorial1.js
+++ 7kyu/factorial1.js
@@ -14,3 +14,15 @@ console.log(factorial(1), 1);
 console.log(factorial(4), 24);
 console.log(factorial(7), 5040);
 console.log(factorial(17), 355687428096000);
+
+const factorial = (n) => (n ? factorial(n - 1) * n : 1);
+function factorial(n) {
+  let answer = 1;
+
+  while (n > 0) {
+    answer *= n;
+    n--;
+  }
+
+  return answer;
+}
