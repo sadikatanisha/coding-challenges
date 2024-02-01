53926711289451ce20eb1750ec4170b64a5e3bda
sadikatanisha <sadikatanisha@gmail.com>
2024-02-01 15:10:59 +0600

    alternative solution

--- 7kyu/likes-vs-dislikes.js
+++ 7kyu/likes-vs-dislikes.js
@@ -13,15 +13,22 @@ function likeOrDislike(buttons) {
 
   return state;
 }
+function likeOrDislike(buttons) {
+  return buttons.reduce(
+    (state, button) => (button === state ? Nothing : button),
+    Nothing
+  );
+}
+
 console.log(likeOrDislike(["Dislike"]), "Dislike");
 console.log(likeOrDislike(["Like", "Like"]), "Nothing");
 console.log(likeOrDislike(["Dislike", "Like"]), "Like");
 console.log(likeOrDislike(["Like", "Dislike", "Dislike"]), "Nothing");
 
 console.log(likeOrDislike(["Dislike", "Dislike"]), "Nothing");
-console.log(likeOrDislike([Like, Like, Like]), Like);
-console.log(likeOrDislike([Like, Dislike]), Dislike);
-console.log(likeOrDislike([Dislike, Like, Dislike]), Dislike);
+// console.log(likeOrDislike([Like, Like, Like]), Like);
+// console.log(likeOrDislike([Like, Dislike]), Dislike);
+// console.log(likeOrDislike([Dislike, Like, Dislike]), Dislike);
 console.log(
   likeOrDislike([
     "Like",
