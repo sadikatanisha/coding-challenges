1054f33bda0530d16ac0d091adf4475cf1e2ecc9
sadikatanisha <sadikatanisha@gmail.com>
2024-02-07 15:00:41 +0600

    like vs dislike

---
+++ 7kyu/likes-vs-dislikes.js
@@ -0,0 +1,38 @@
+// Like, Dislike, Nothing come from Preloaded
+
+function likeOrDislike(buttons) {
+  let state = "Nothing";
+
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i] === state) {
+      state = "Nothing";
+    } else {
+      state = buttons[i];
+    }
+  }
+
+  return state;
+}
+console.log(likeOrDislike(["Dislike"]), "Dislike");
+console.log(likeOrDislike(["Like", "Like"]), "Nothing");
+console.log(likeOrDislike(["Dislike", "Like"]), "Like");
+console.log(likeOrDislike(["Like", "Dislike", "Dislike"]), "Nothing");
+
+console.log(likeOrDislike(["Dislike", "Dislike"]), "Nothing");
+console.log(likeOrDislike([Like, Like, Like]), Like);
+console.log(likeOrDislike([Like, Dislike]), Dislike);
+console.log(likeOrDislike([Dislike, Like, Dislike]), Dislike);
+console.log(
+  likeOrDislike([
+    "Like",
+    "Like",
+    "Dislike",
+    "Like",
+    "Like",
+    "Like",
+    "Like",
+    "Dislike",
+  ]),
+  "Dislike"
+);
+console.log(likeOrDislike([]), "Nothing");
