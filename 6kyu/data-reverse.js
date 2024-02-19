78b6392299263d37dba4d18d18c5b51814680dc9
sadikatanisha <sadikatanisha@gmail.com>
2024-02-19 12:09:40 +0600

    data reverse using unshift

--- 6kyu/data-reverse.js
+++ 6kyu/data-reverse.js
@@ -9,6 +9,15 @@ function dataReverse(data) {
   return concat;
 }
 
+const dataReverse = (data) => {
+  const bytes = [];
+  for (let i = 0; i < data.length; i += 8) {
+    bytes.unshift(...data.slice(i, i + 8));
+    console.log(bytes);
+  }
+  return bytes;
+};
+
 const data1 = [
   1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
   1, 0, 1, 0, 1, 0,
