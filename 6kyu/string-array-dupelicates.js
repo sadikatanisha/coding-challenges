c57b8a8de602030213c9d145f271f552133dd47f
sadikatanisha <sadikatanisha@gmail.com>
2024-02-24 15:19:55 +0600

    string array dupes

--- 6kyu/string-array-dupelicates.js
+++ 6kyu/string-array-dupelicates.js
@@ -59,3 +59,14 @@ console.log(
 //   "voradospulen",
 //   "achcha",
 // ]);
+
+function dup(s) {
+  return s.map((w) => {
+    return w
+      .split("")
+      .filter((c, i, arr) => {
+        return c !== arr[i - 1];
+      })
+      .join("");
+  });
+}
