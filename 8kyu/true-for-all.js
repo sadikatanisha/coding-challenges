4a83d2896b1ee6722168ef9f7312c8ad22f64fdb
sadikatanisha <sadikatanisha@gmail.com>
2024-07-26 01:47:44 +0600

    true for all

---
+++ 8kyu/true-for-all.js
@@ -0,0 +1,16 @@
+function all(arr, fun) {
+  return arr.every((el) => fun(el));
+}
+
+console.log(
+  all([1, 2, 3, 4, 5], function (v) {
+    return v < 9;
+  }),
+  true
+);
+console.log(
+  all([1, 2, 3, 4, 5], function (v) {
+    return v > 9;
+  }),
+  false
+);
